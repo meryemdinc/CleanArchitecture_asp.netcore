9c6f1317467677a4f75f8c98c71687e5feb0a06f
meryemdinc <meryemdinc45@gmail.com>
2025-10-15 23:32:34 +0300

    feat(services): add ProductDto record with primary constructor

---
+++ Services/Products/ProductDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.Products
+{
+    public record ProductDto(int Id, string Name, decimal Price, int Stock);//primary constructor
+
+    /* public record ProductDto
+      {
+          public int Id { get; init; }
+          public string Name { get; init; } 
+          public decimal Price { get; init; }
+
+          public int Stock { get; init; }
+          //init sadece nesne oluşturulurken set edilebilir,
+          //sonradan nesnemiz değiştirilemesin diye böyle yaptık
+      }*/
+}
