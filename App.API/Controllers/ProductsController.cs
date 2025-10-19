9c23b10812ab6d527286751ba350a040a5fbabd5
meryemdinc <meryemdinc45@gmail.com>
2025-10-19 13:32:03 +0300

    feat(controller): add "{id}" route parameter to Update method in ProductsController

--- App.API/Controllers/ProductsController.cs
+++ App.API/Controllers/ProductsController.cs
@@ -10,14 +10,15 @@ namespace App.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()=>CreateActionResult(await productService.GetAllListAsync());
      
-        [HttpGet)]
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)=> CreateActionResult( await productService.GetByIdAsync(id));
         [HttpPost]
         public async Task<IActionResult> Create(CreateProductRequest request) => CreateActionResult(await productService.CreateAsync(request));
 
-        [HttpPut]
+        [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, UpdateProductRequest request) => CreateActionResult(await productService.UpdateAsync(id, request));
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id) => CreateActionResult(await productService.DeleteAsync(id));
     }
 }
+//("{id}") ekleyerek bu id'nin route'dan geleceğini belirtiyoruz.
