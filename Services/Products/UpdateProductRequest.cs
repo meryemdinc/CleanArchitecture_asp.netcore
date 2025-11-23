a7ec599fe011747e45fc404dbb8e65e0cdf257b9
meryemdinc <meryemdinc45@gmail.com>
2025-11-23 23:55:43 +0300

    refactor: UpdateProductRequest.cs is moved

--- Services/Products/UpdateProductRequest.cs
+++
@@ -1,4 +0,0 @@
-﻿
-namespace App.Services.Products;
-public record UpdateProductRequest( string Name, decimal Price, int Stock);
-//int id parametresine gerek yok çünkü zaten servis katmanında id'yi parametre olarak alıyoruz ve orada güncelleme yaparken kullanıyoruz.
