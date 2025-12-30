c0c03d57a288216a9608e9dddf50af7bdf09a3f4
meryemdinc <meryemdinc45@gmail.com>
2025-12-30 22:34:45 +0300

    docs(product): add comments explaining nullable string handling

--- Repositories/Products/Product.cs
+++ Repositories/Products/Product.cs
@@ -3,8 +3,9 @@
    public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = default!;
-        public decimal Price{ get; set; }
+    public string Name { get; set; } = default!;//public required string Name { get; set; } .net 8 ile gelen özellik bizim yazdığımızın yerine bu tarz da yazılabilir default! yerine
+    //default! kullanmamızın sebebi string? demedik yani null olabilir demedik bu yüzden string in default degeri null biz de default! diyerek ileride bu property'sinin null olmayacağını belirttik.
+    public decimal Price{ get; set; }
         public int Stock{ get; set; }
     }
 
