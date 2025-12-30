0f8a21ac99a4b6b81b5efefb0d4f5f09772ec711
meryemdinc <meryemdinc45@gmail.com>
2025-12-30 22:36:10 +0300

    refactor(repositories): convert to file-scoped namespace and add comments

--- Repositories/GenericRepository.cs
+++ Repositories/GenericRepository.cs
@@ -1,18 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using App.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
-namespace App.Repositories
-{
-    public class GenericRepository<T>(AppDbContext context) : IGenericRepository<T> where T : class
-    {
-        protected AppDbContext Context = context;//protected miras alınan classlarda kullanılır.
-        private readonly DbSet<T> _dbSet=context.Set<T>();//readonlyde sadece 1 kere set edebilirsin,değer atayabilirsin.
-        public IQueryable<T> GetAll()=> _dbSet.AsQueryable().AsNoTracking();//memory i şişirmemek için asnotracking
-        public IQueryable<T> Where(Expression<Func<T, bool>> predicate)=> _dbSet.Where(predicate).AsQueryable().AsNoTracking();
-        public ValueTask<T?> GetByIdAsync(int id) => _dbSet.FindAsync(id);   
-        public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);
-        public void Update(T entity) => _dbSet.Update(entity);
-        public void Delete(T entity)=>_dbSet.Remove(entity);
+namespace App.Repositories;
 
-        //AsNoTracking() veri üzerinde değişiklik yapılmayacaksa sadece veriyi çekeceksen performans açısından faydalıdır.
-    }
+public class GenericRepository<T>(AppDbContext context) : IGenericRepository<T> where T : class
+{
+        protected AppDbContext Context = context;//protected miras alınan classlarda kullanılır.
+    private readonly DbSet<T> _dbSet = context.Set<T>();//readonlyde sadece 1 kere set edebilirsin,değer atayabilirsin.
+    public IQueryable<T> GetAll() => _dbSet.AsQueryable().AsNoTracking();//memory i şişirmemek için asnotracking
+    public IQueryable<T> Where(Expression<Func<T, bool>> predicate) => _dbSet.Where(predicate).AsQueryable().AsNoTracking();
+    public ValueTask<T?> GetByIdAsync(int id) => _dbSet.FindAsync(id);
+    public async ValueTask AddAsync(T entity) => await _dbSet.AddAsync(entity);//async await kullanımı sebebi,AddAsync methodunun ValueTask döndürmemesi
+                                                                              //ValueTask,<T> 'siz ise void in asenkron hali gibi düşünebilirsin.
+    public void Update(T entity) => _dbSet.Update(entity);
+    public void Delete(T entity) => _dbSet.Remove(entity);
+
+    //AsNoTracking() veri üzerinde değişiklik yapılmayacaksa sadece veriyi çekeceksen performans açısından faydalıdır.
+
 }
